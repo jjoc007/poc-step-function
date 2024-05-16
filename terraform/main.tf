42da69cfd09fa826cc53e91c8e67efeac6d5c722
Juan Orjuela <juanjose.orjuela@mercadolibre.com.co>
2024-05-15 23:18:35 -0500

    ajustes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,9 @@
-provider "aws" {
-  version    = "~> 3.0"
-  region     = "us-east-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    archive = {}
+  }
 }
-
-provider "archive" {}
